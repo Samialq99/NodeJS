a87ccc821b154c84f90b8e435b46db55638c94ca
Sami Alq <sarmad99@msn.com>
2020-01-31 00:57:56 -0500

    NodeJS baby

---
+++ firstFile.js
@@ -0,0 +1,5 @@
+console.log("Hello world");
+let hello = "Hello Node JS from a string";
+console.log(hello);
+// the console is part of the global object so we can type it like
+global.console.log("Global object", hello);
---
+++ global.js
@@ -0,0 +1,2 @@
+console.log(__dirname, "     This will Print path to current dir");
+console.log(__filename, "     This will Print path to current file");
